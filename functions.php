73c3b90e3078fd0e2872faa171562bbfa4335726
Alessio <pangosalessio@gmail.com>
2018-12-14 09:40:15 +0100

    responsive artist archive

--- functions.php
+++ functions.php
@@ -22,7 +22,7 @@ include_once (get_template_directory().'/lib/init.php');
 // Define theme constants.
 define('CHILD_THEME_NAME', 'Business Pro Theme');
 define('CHILD_THEME_URL', 'https://seothemes.com/themes/business-pro');
-define('CHILD_THEME_VERSION', '1.0.5.2018-08-08-a14');
+define('CHILD_THEME_VERSION', '1.0.5.2018-08-08-a15');
 
 // Set Localization (do not remove).
 load_child_theme_textdomain('business-pro-theme', apply_filters('child_theme_textdomain', get_stylesheet_directory().'/languages', 'business-pro-theme'));
